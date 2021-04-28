d00e47c754b979e9ef63a1ce5b6fbed42f2cd147
RobertoPotato <robertwabuti@gmail.com>
2021-04-28 12:16:25 +0300

    Day-24, Saving the High Score in the snake game.

--- Day_24/scoreboard.py
+++ Day_24/scoreboard.py
@@ -13,6 +13,7 @@ class ScoreBoard(Turtle):
         self.goto(0, 265)
         self.hideturtle()
         self.update_scoreboard()
+        self.read_high_score()
 
     def update_scoreboard(self):
         self.clear()
@@ -21,12 +22,23 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.save_high_score()
         self.score = 0
         self.update_scoreboard()
 
     def increase_score(self):
         self.score += 10
         self.update_scoreboard()
+
+    def save_high_score(self):
+        with open("data.txt", "w") as file:
+            file.write(str(self.high_score))
+
+    def read_high_score(self):
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+            self.update_scoreboard()
+
 
 
 
