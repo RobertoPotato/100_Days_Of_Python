f21cf3f502803275e832d99e4ce83a2779c5396a
RobertoPotato <robertwabuti@gmail.com>
2021-04-28 11:54:50 +0300

    Day-24, Keeping track of High Score in the snake game.

---
+++ Day_24/scoreboard.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 265)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 10
+        self.update_scoreboard()
+
+
+
