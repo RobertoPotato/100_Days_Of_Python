9d8319a70c17d86412b6901209245cb46086d24e
bob <robertwabuti@gmail.com>
2021-08-30 23:33:14 +0300

    Day-29: UI Setup for password manager

--- Day_29/29main.py
+++ Day_29/29main.py
@@ -1,9 +1,16 @@
 from tkinter import *
+delimiter = "|"
 def action():
     pass
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_pwd():
+    # key_data = site + delimiter + email + delimiter + pwd + "\n"
+    key_data = "site + delimiter + ===========d"
+    with open("passwords.txt", 'w') as pwd_file:
+        pwd_file.write(key_data)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -21,12 +28,14 @@ canvas.grid(row=0, column=1)
 webs_label = Label(text="Website:")
 webs_label.grid(row=1, column=0)
 webs_entry = Entry(width=52)
+webs_entry.focus()
 webs_entry.grid(row=1, column=1, columnspan=2)
 
 # EMAIL/USERNAME SECTION
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
 email_entry = Entry(width=52)
+email_entry.insert(0, "robbs@gmail.com")
 email_entry.grid(row=2, column=1, columnspan=2)
 
 # PASSWORD SECTION
@@ -36,8 +45,9 @@ pw_entry = Entry(width=52)
 pw_entry.grid(row=3, column=1, columnspan=2)
 button = Button(text="Generate Password", width=15, highlightthickness=0, command=action)
 button.grid(row=3, column=2)
+print("PWD: ", pw_entry)
 
 # ADD BUTTON SECTION
-button = Button(text="Add", command=action, width=45, )
+button = Button(text="Add", command=save_pwd, width=45, )
 button.grid(row=4, column=1, columnspan=2)
 window.mainloop()
